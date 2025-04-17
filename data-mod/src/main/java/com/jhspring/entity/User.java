a61e151a850a1a12e7e3d2b7433950833436e503
minod <minod@naver.com>
2025-04-17 21:55:35 +0900

    data-mod mariadbset

---
+++ data-mod/src/main/java/com/jhspring/entity/User.java
@@ -0,0 +1,31 @@
+package com.jhspring.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+
+    private String firstName;
+    private String lastName;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private boolean active;
+}
