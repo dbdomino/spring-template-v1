258f737b59990dc48da498b383177b9e3ea6dc6f
minod <minod@naver.com>
2025-04-26 15:58:48 +0900

    멀티모듈 기반 설계, 실행파일, scan 범위 추가.

--- app-mod/user-app/src/main/java/com/jhspring/UserAppMain.java
+++ app-mod/user-app/src/main/java/com/jhspring/UserAppMain.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.jhspring.data",
+        "com.jhspring.core",
+        "com.jhspring.common",
+        "com.jhspring.user"
+})
 @ConfigurationPropertiesScan
 public class UserAppMain {
     public static void main(String[] args) {
