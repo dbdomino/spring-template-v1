8bb0f8dabb915729af56d8460bbc5b09314a2a9b
minod <minod@naver.com>
2025-04-26 17:15:41 +0900

    수정

--- core-mod/src/main/java/com/jhspring/core/config/OrderProperties.java
+++ core-mod/src/main/java/com/jhspring/core/config/OrderProperties.java
@@ -3,8 +3,6 @@ package com.jhspring.core.config;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
-import org.springframework.stereotype.Component;
 
 @Getter
 @RequiredArgsConstructor
