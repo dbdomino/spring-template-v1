41917b9f112610d18753397990e93b8ad3085471
dbdomino <dbdomino@naver.com>
2025-05-04 22:07:45 +0900

    로그인 모델로 dto 매퍼 작성

--- app-mod/user-app/src/main/java/com/jhspring/mapper/LoginMapper.java
+++ app-mod/user-app/src/main/java/com/jhspring/mapper/LoginMapper.java
@@ -4,9 +4,12 @@ import com.jhspring.core.user.LoginUser;
 import com.jhspring.dto.req.LoginReqDto;
 import com.jhspring.dto.res.LoginResDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface LoginMapper {
+    LoginMapper INSTANCE = Mappers.getMapper(LoginMapper.class);
+
     LoginReqDto toRequestDto(LoginUser loginUser);
     LoginResDto toResponseDto(LoginUser loginUser);
 }
