12833b0de7965032be76bbbc7304a3f5e24195d9
dbdomino <dbdomino@naver.com>
2025-05-05 00:15:14 +0900

    속성중에 id로 조회하는 명령 추가(data jpa 명령이라 잘모르겟)

--- data-mod/src/main/java/com/jhspring/data/repository/UserRepository.java
+++ data-mod/src/main/java/com/jhspring/data/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.jhspring.data.repository;
 import com.jhspring.data.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository  extends JpaRepository<UserEntity, Long> {
+    boolean existsById(String id); // Entity의 login ID 기준 존재 여부
+    Optional<UserEntity> findById(String id); // user Id 로 조회하기.
 }
