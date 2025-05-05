8b5d16d936309595fb888aef419f1bcd60c34428
dbdomino <dbdomino@naver.com>
2025-05-05 23:41:12 +0900

    컨트롤러 분리

--- app-mod/front-app/src/main/java/com/jhspring/front/controller/UserPageController.java
+++ app-mod/front-app/src/main/java/com/jhspring/front/controller/UserPageController.java
@@ -1,9 +1,6 @@
 package com.jhspring.front.controller;
 
-import com.jhspring.common.constants.ErrorCode;
-import com.jhspring.front.dto.req.LoginReqDto;
 import com.jhspring.front.dto.req.RegistUserReqDto;
-import com.jhspring.front.dto.res.LoginResDto;
 import com.jhspring.front.dto.res.RegistUserResDto;
 import com.jhspring.front.service.itf.UserPageService;
 import jakarta.servlet.http.HttpSession;
@@ -19,11 +16,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class UserPageController {
     private final UserPageService userPageService;
 
-    // 로그인 페이지
-    @GetMapping("/login")
-    public String loginPage() {
-        return "user/login";
-    }
     // 회원가입 페이지
     @GetMapping("/signup")
     public String signupPage() {
@@ -45,22 +37,7 @@ public class UserPageController {
         return "user/dashboard";
     }
 
-    // 로그인 처리
-    @PostMapping("/login")
-    public String loginProcess(LoginReqDto req, HttpSession session, RedirectAttributes redirect) {
-        LoginResDto res = userPageService.login(req, session);
-        if (res == null) {
-            redirect.addFlashAttribute("code", ErrorCode.LOGIN_FAIL.getMessage());
-            redirect.addFlashAttribute("message", ErrorCode.LOGIN_FAIL.getMessage());
-            return "redirect:/login";
-        }
 
-        session.setAttribute("loginUser", res.getId());
-        session.setAttribute("loginUserName", res.getName());
-        session.setAttribute("loginUserEmail", res.getEmail());
-
-        return "redirect:/dashboard";
-    }
 
     @PostMapping("/signup")
     public String signup(RegistUserReqDto req, RedirectAttributes redirect) {
