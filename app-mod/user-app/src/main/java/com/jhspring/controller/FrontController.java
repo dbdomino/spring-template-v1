c735f19047aa262cdba5057b90c87d6ef8de5eb6
dbdomino <dbdomino@naver.com>
2025-05-05 22:38:20 +0900

    [user-app] 안쓰는 파일 삭제

--- app-mod/user-app/src/main/java/com/jhspring/controller/FrontController.java
+++
@@ -1,9 +0,0 @@
-package com.jhspring.controller;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-
-@Controller
-@RequiredArgsConstructor
-public class FrontController {
-}
