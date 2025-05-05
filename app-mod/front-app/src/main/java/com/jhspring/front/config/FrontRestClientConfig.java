411d05691572f3b7b838c0d3613bf36141782077
dbdomino <dbdomino@naver.com>
2025-05-06 01:18:05 +0900

    rest client 직접생성

--- app-mod/front-app/src/main/java/com/jhspring/front/config/FrontRestClientConfig.java
+++ app-mod/front-app/src/main/java/com/jhspring/front/config/FrontRestClientConfig.java
@@ -4,13 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
-@Configuration
+//@Configuration
 public class FrontRestClientConfig {
-
+/*
     @Bean
     public RestClient userAppRestClient(RestClient.Builder builder) {
         return builder
                 .baseUrl("http://localhost:8081")
                 .build();
     }
+
+ */
 }
